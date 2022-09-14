1c73a82ba27a91705304c5ed093f0642d403d25c
Kashin Nikita <kashin.nik008@gmail.com>
2022-09-14 20:17:26 +0300

    Replace some if..else blocks on match..case block. #1

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -17,36 +17,29 @@ class Lexer:
         self.current_state = self.states.firstLitera
         
     def main(self, symbol):
-        if self.current_state == self.states.firstLitera:
-            if (symbol >= 'a' and symbol <= 'z') or (symbol >= 'A' and symbol <= 'Z'):
-                self.current_state = self.states.nextLitera
-                self.token.append(symbol)
-                return
-            else:
-                self.current_state = self.states.error
-                return
+        match self.current_state:
+            case self.states.firstLitera:
+                if (symbol >= 'a' and symbol <= 'z') or (symbol >= 'A' and symbol <= 'Z'):
+                    self.current_state = self.states.nextLitera
+                    self.token.append(symbol)
+                else:
+                    self.current_state = self.states.error
 
-        if self.current_state == self.states.nextLitera:
-            if symbol == ' ' or symbol == '\n':
-                self.current_state = self.states.firstLitera
-                print("".join(self.token))
-                self.token = []
-                return
-            elif (symbol >= 'a' and symbol <= 'z') or (symbol >= 'A' and symbol <= 'Z') or (symbol >= '0' and symbol <= '9'):
-                self.token.append(symbol)
-                return
-            else:
-                self.current_state = self.states.error
-                return
-
-        if self.current_state == self.states.error:
-            if symbol == ' ' and symbol != '\n':
-                self.token = []
-                print("Error")
-                self.current_state = self.states.firstLitera
-                return
-            else:
-                return
+            case self.states.nextLitera:
+                if symbol == ' ' or symbol == '\n':
+                    self.current_state = self.states.firstLitera
+                    print("".join(self.token))
+                    self.token = []
+                elif (symbol >= 'a' and symbol <= 'z') or (symbol >= 'A' and symbol <= 'Z') or (symbol >= '0' and symbol <= '9'):
+                    self.token.append(symbol)
+                else:
+                    self.current_state = self.states.error
+            
+            case self.states.error:
+                if symbol == ' ' and symbol != '\n':
+                    self.token = []
+                    print("Error")
+                    self.current_state = self.states.firstLitera
        
 lex = Lexer()
 with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'text.txt')) as file:
