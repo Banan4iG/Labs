98477c903bc1089b89d7daca96f349ee1b57f3eb
Kashin Nikita <kashin.nik008@gmail.com>
2022-09-14 21:33:51 +0300

    Add function for everyone state. #3
    Also was added suport for Cyrillic characters

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -1,6 +1,6 @@
 from enum import Enum
 import os
-
+import codecs
 
 class States(Enum):
     firstLitera = 'firstLitera'
@@ -15,37 +15,47 @@ class Lexer:
         self.states = States
         self.token = []
         self.current_state = self.states.firstLitera
-        
+
+    def firstLitera_action(self, symbol):
+        if symbol.isalpha():
+            self.current_state = self.states.nextLitera
+            self.token.append(symbol)
+        else:
+            self.current_state = self.states.error
+
+    def nextLitera_action(self, symbol):
+        if symbol == ' ' or symbol == '\n':
+            self.current_state = self.states.firstLitera
+            print("".join(self.token))
+            self.token = []
+        elif symbol.isalpha() or symbol.isdecimal():
+            self.token.append(symbol)
+        else:
+            self.current_state = self.states.error
+
+    def error_action(self, symbol):
+        if symbol == ' ' or symbol == '\n':
+            self.token = []
+            print("Error")
+            self.current_state = self.states.firstLitera
+
     def main(self, symbol):
         match self.current_state:
             case self.states.firstLitera:
-                if (symbol >= 'a' and symbol <= 'z') or (symbol >= 'A' and symbol <= 'Z'):
-                    self.current_state = self.states.nextLitera
-                    self.token.append(symbol)
-                else:
-                    self.current_state = self.states.error
+                self.firstLitera_action(symbol)
 
             case self.states.nextLitera:
-                if symbol == ' ' or symbol == '\n':
-                    self.current_state = self.states.firstLitera
-                    print("".join(self.token))
-                    self.token = []
-                elif (symbol >= 'a' and symbol <= 'z') or (symbol >= 'A' and symbol <= 'Z') or (symbol >= '0' and symbol <= '9'):
-                    self.token.append(symbol)
-                else:
-                    self.current_state = self.states.error
+                self.nextLitera_action(symbol)
             
             case self.states.error:
-                if symbol == ' ' and symbol != '\n':
-                    self.token = []
-                    print("Error")
-                    self.current_state = self.states.firstLitera
-       
+                self.error_action(symbol)
+
+symbols = []
 lex = Lexer()
-with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'text.txt')) as file:
+with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'text.txt'), encoding = "utf-8") as file:
     for symbol in file.read():
         lex.main(symbol)
-    lex.current_state = lex.states.stop
 
+lex.current_state = lex.states.stop
 if lex.current_state == lex.states.stop:
     print("Stop")
